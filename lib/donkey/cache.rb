c61c4e8ea4709431ecaa1b4b24de0d44d447c1d7
vkcodes <vincas@cgtrader.com>
2018-10-18 11:16:25 +0300

    Improve caching

--- lib/donkey/cache.rb
+++ lib/donkey/cache.rb
@@ -1,7 +1,30 @@
 module Donkey
+  module ThreadCache
+    def self.write(*args, &block)
+      key, val = args
+
+      hash_in_current_thread[key] = val
+    end
+
+    def self.read(*args, &block)
+      key, _val = args
+
+      hash_in_current_thread[key]
+    end
+
+    def self.clear
+      Thread.current[:cache] = {}
+    end
+
+    def self.hash_in_current_thread
+      Thread.current[:cache] ||= {}
+    end
+  end
+
   module NullCache
     def self.write(*); end
     def self.read(*); end
+    def self.clear; end
   end
 
   module Cache
--- spec/donkey_client/services/assign_alternative_spec.rb
+++ spec/donkey_client/services/assign_alternative_spec.rb
@@ -5,8 +5,9 @@ describe DonkeyClient::Services::AssignAlternative do
 
   let(:experiment_slug) { 'exp_slug' }
   let(:anonymous_user_id) { 'auid123' }
-  let(:cache) { Donkey::NullCache }
   let(:user_id) { nil }
+  let(:cache) { Donkey::ThreadCache }
+  let(:cache_key) { "donkey/#{experiment_slug}/#{anonymous_user_id}/#{user_id}" }
 
   context 'when user is bot' do
     let(:is_bot) { true }
@@ -26,11 +27,71 @@ describe DonkeyClient::Services::AssignAlternative do
 
     before do
       allow_any_instance_of(::Donkey::Settings).to receive(:always_control_group?).and_return(false)
-      allow_any_instance_of(described_class).to receive(:alternative).and_return('alternative')
+      allow_any_instance_of(described_class).to receive(:response_body).and_return(response_body_hash)
+      allow_any_instance_of(described_class).to receive(:control_group).and_return('control_group')
     end
 
-    it 'returns assigned alternative' do
-      expect(subject).to eq('alternative')
+    context 'when cache is empty' do
+      before { cache.clear }
+
+      context 'when response body contains data with alternative slug' do
+        let(:response_body_hash) { { 'data' => 'alternative' } }
+
+        it 'returns assigned alternative' do
+          expect(subject).to eq('alternative')
+        end
+
+        it 'writes data to cache' do
+          expect { subject }.to change { cache.read(cache_key) }.from(nil).to('alternative')
+        end
+
+        it 'requests data from `donkey_dashboard`' do
+          expect_any_instance_of(described_class).to receive(:response_body)
+          subject
+        end
+      end
+    end
+
+    context 'when cache contains alternative for given key' do
+      before { cache.write(cache_key, 'alternative') }
+
+      context 'when response body contains data with alternative slug' do
+        let(:response_body_hash) { { 'data' => 'alternative' } }
+
+        it 'returns assigned alternative' do
+          expect(subject).to eq('alternative')
+        end
+
+        it 'does not write data to cache' do
+          expect(cache).not_to receive(:write)
+          subject
+        end
+
+        it 'does not request data from `donkey_dashboard`' do
+          expect_any_instance_of(described_class).not_to receive(:response_body)
+          subject
+        end
+      end
+    end
+
+    context 'when response is empty' do
+      before { cache.clear }
+
+      let(:response_body_hash) { { } }
+
+      it 'returns assigned alternative' do
+        expect(subject).to eq('control_group')
+      end
+
+      it 'notifies about missing data' do
+        expect(Donkey).to receive(:notify).with(instance_of(KeyError))
+        subject
+      end
+
+      it 'does not write data to cache' do
+        expect(cache).not_to receive(:write)
+        subject
+      end
     end
   end
 
@@ -39,6 +100,7 @@ describe DonkeyClient::Services::AssignAlternative do
     let(:is_always_control_group) { true }
 
     before do
+      cache.clear
       allow_any_instance_of(::Donkey::Settings).to receive(:always_control_group?).and_return(true)
       allow_any_instance_of(described_class).to receive(:control_group).and_return('control_group')
     end
