4eab551a5251e913e94288f0f1878fdd24ef01f4
Aurimas Šeputis <inbox@rbviz.com>
2019-01-10 14:44:57 +0200

    Update donkey_client minor version again

--- lib/donkey_client/service_workers.rb
+++ lib/donkey_client/service_workers.rb
@@ -1,6 +1,7 @@
 require_relative 'service_workers/base'
 require_relative 'service_workers/track'
 require_relative 'service_workers/reject'
+require_relative 'service_workers/change_alternative'
 
 module DonkeyClient
   module ServiceWorkers; end
--- lib/donkey_client/version.rb
+++ lib/donkey_client/version.rb
@@ -1,3 +1,3 @@
 module DonkeyClient
-  VERSION = '0.4.1'.freeze
+  VERSION = '0.4.2'.freeze
 end
