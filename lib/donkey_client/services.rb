6dfc1c29a129e6addfedeae6995276ae37748115
Aurimas Šeputis <daftviz@gmail.com>
2019-01-10 10:30:23 +0200

    Feature/25727 reparticipate function (#22)
    * Implement reparticipate action
    
    * Add change alternative to services
    
    * Add change alternative to services
    
    * Add success scenario

--- lib/donkey_client/services.rb
+++ lib/donkey_client/services.rb
@@ -2,6 +2,7 @@ require_relative 'services/base'
 require_relative 'services/track'
 require_relative 'services/reject'
 require_relative 'services/assign_alternative'
+require_relative 'services/change_alternative'
 
 module DonkeyClient
   module Services; end
---
+++ spec/donkey_client/services/change_alternative_spec.rb
@@ -0,0 +1,40 @@
+describe DonkeyClient::Services::ChangeAlternative do
+  subject { described_class.execute(experiment_slug, alternative_slug, donkey_context) }
+
+  let(:experiment_slug) { 'exp_slug' }
+  let(:alternative_slug) { 'alt_slug' }
+  let(:is_bot) { false }
+
+  let(:donkey_context) do
+    double(
+      anonymous_user_id: 'auid123',
+      is_bot: is_bot,
+      user_id: nil,
+      new_visitor: nil
+    )
+  end
+
+  let(:expected_response) { double(body: { data: alternative_slug }.to_json, code: 200) }
+
+  context 'when user is not a bot' do
+    before do
+      expect(DonkeyClient::Resource::Alternative).to receive(:post) { expected_response }
+    end
+
+    it 'calls external resource and returns selected alternative slug' do
+      is_expected.to eq(alternative_slug)
+    end
+  end
+
+  context 'when user is bot' do
+    let(:is_bot) { true }
+
+    before do
+      expect(DonkeyClient::Resource::Alternative).not_to receive(:post)
+    end
+
+    it 'does not call external resource' do
+      is_expected.to be_nil
+    end
+  end
+end
